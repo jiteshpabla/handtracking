bc807da9d2135afe73494d130ec44b9610eebf2d
Jitesh Pabla <jiteshpabla97@gmail.com>
2020-08-12 20:24:20 -0700

    Update detect_single_threaded.py

--- detect_single_threaded.py
+++ detect_single_threaded.py
@@ -64,6 +64,18 @@ if __name__ == '__main__':
         type=int,
         default=5,
         help='Size of the queue.')
+    parser.add_argument(
+        '-frame',
+        '--frame_path',
+        dest='frame_path',
+        default=0,
+        help='Frames folder path.')
+    parser.add_argument(
+        '-filename',
+        '--video',
+        dest='video_name',
+        default=0,
+        help='Video name.')
     args = parser.parse_args()
 
     cap = cv2.VideoCapture(args.video_source)
@@ -75,47 +87,43 @@ if __name__ == '__main__':
     im_width, im_height = (cap.get(3), cap.get(4))
     # max number of hands we want to detect/track
     num_hands_detect = 2
+    
+    #cannot open window in google colab
+    #cv2.namedWindow('Single-Threaded Detection', cv2.WINDOW_NORMAL)
 
-    cv2.namedWindow('Single-Threaded Detection', cv2.WINDOW_NORMAL)
-
-    while True:
+    ret= True
+    count=0
+    while ret:
         # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        #frame may not be read properly; a do-while type implemetation to avoid crashes
         ret, image_np = cap.read()
-        # image_np = cv2.flip(image_np, 1)
-        try:
-            image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-        except:
-            print("Error converting to RGB")
+        #to save the original image
+        temp_image= image_np
+        if ret:
+            # image_np = cv2.flip(image_np, 1)
+            if not image_np is None:
+                try:
+                    image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
+                    #print("converting to RGB")
+                except:
+                    print("Error converting to RGB")
 
-        # Actual detection. Variable boxes contains the bounding box cordinates for hands detected,
-        # while scores contains the confidence for each of these boxes.
-        # Hint: If len(boxes) > 1 , you may assume you have found atleast one hand (within your score threshold)
+            # Actual detection. Variable boxes contains the bounding box cordinates for hands detected,
+            # while scores contains the confidence for each of these boxes.
+            # Hint: If len(boxes) > 1 , you may assume you have found atleast one hand (within your score threshold)
 
-        boxes, scores = detector_utils.detect_objects(image_np,
-                                                      detection_graph, sess)
+            boxes, scores = detector_utils.detect_objects(image_np,
+                                                          detection_graph, sess)
+            
+            count += 1
+            # draw bounding boxes on frame
+            detector_utils.draw_box_on_image(num_hands_detect, args.score_thresh,
+                                             scores, boxes, im_width, im_height,
+                                             image_np, temp_image, count, args.frame_path, args.video_name)
 
-        # draw bounding boxes on frame
-        detector_utils.draw_box_on_image(num_hands_detect, args.score_thresh,
-                                         scores, boxes, im_width, im_height,
-                                         image_np)
-
-        # Calculate Frames per second (FPS)
-        num_frames += 1
-        elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
-        fps = num_frames / elapsed_time
-
-        if (args.display > 0):
-            # Display FPS on frame
-            if (args.fps > 0):
-                detector_utils.draw_fps_on_image("FPS : " + str(int(fps)),
-                                                 image_np)
-
-            cv2.imshow('Single-Threaded Detection',
-                       cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
-
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                cv2.destroyAllWindows()
-                break
+            # Calculate Frames per second (FPS)
+            num_frames += 1
+            elapsed_time = (datetime.datetime.now() - start_time).total_seconds()
+            fps = num_frames / elapsed_time
         else:
-            print("frames processed: ", num_frames, "elapsed time: ",
-                  elapsed_time, "fps: ", str(int(fps)))
+            continue
